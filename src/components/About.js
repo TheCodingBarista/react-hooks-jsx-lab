8b9996dbe217b54cefc04b8499ed7f2f7bbb3fcb
+ <+>
2021-11-02 20:25:18 -0700

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>My name is Mel, and I think I'm pretty dope. So does my lady.</p>
+    <img src={ image } alt={ "I made this"}></img>
+  </div>;
 }
 
 export default About;
